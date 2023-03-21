954a1e72d22b7714f6b996d4d9a664e29c0deed2
ArsalanWebDev <Arsalan.Youneszadehbavani@student.intecbrussel.be>
2023-03-21 13:14:47 +0100

    ice

---
+++ index.js
@@ -0,0 +1,10 @@
+const monBody = document.querySelector("body");
+const image = document.getElementById("icecream-image");
+const price = document.getElementById("price");
+
+const potSelector = document.querySelector("#pot");
+const imgselector = document.querySelector("#bigPot");
+
+function check() {
+  document.querySelector("#pot").checked = alert("hello");
+}
